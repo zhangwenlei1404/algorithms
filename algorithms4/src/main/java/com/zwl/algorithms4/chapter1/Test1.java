b392bc1d29071de4b68c29b125dc9333d54314a9
za-zhangwenlei <wenlei.zhang@zatech.com>
2021-09-24 09:53:50 +0800

    algorithms4 test commit

---
+++ algorithms4/src/main/java/com/zwl/algorithms4/chapter1/Test1.java
@@ -0,0 +1,9 @@
+package com.zwl.algorithms4.chapter1;
+
+/**
+ * @Description:
+ * @Author: za-zhangwenlei
+ * @Date: Created in 9:50 2021/9/24
+ */
+public class Test1 {
+}
---
+++ algorithms4/src/main/java/com/zwl/algorithms4/chapter2/Test2.java
@@ -0,0 +1,9 @@
+package com.zwl.algorithms4.chapter2;
+
+/**
+ * @Description:
+ * @Author: za-zhangwenlei
+ * @Date: Created in 9:51 2021/9/24
+ */
+public class Test2 {
+}

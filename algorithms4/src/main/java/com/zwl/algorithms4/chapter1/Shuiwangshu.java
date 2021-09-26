0a707d47b5de5374918e5a77376f31e0b4217c95
za-zhangwenlei <wenlei.zhang@zatech.com>
2021-09-26 10:23:30 +0800

    水王数算法提交

---
+++ algorithms4/src/main/java/com/zwl/algorithms4/chapter1/Shuiwangshu.java
@@ -0,0 +1,11 @@
+package com.zwl.algorithms4.chapter1;
+
+/**
+ * @Description:
+ * @Author: za-zhangwenlei
+ * @Date: Created in 11:24 2021/9/24
+ */
+public class Shuiwangshu {
+
+    //水王数 同一个数字存在一半以上，即为水王数
+}
--- leetcode/src/main/java/com/zwl/leetcode/Test1.java
+++ leetcode/src/main/java/com/zwl/leetcode/Test1.java
@@ -1,11 +1,11 @@
-package com.zwl.algorithms4.chapter2;
+package com.zwl.leetcode;
 
 /**
  * @Description:
  * @Author: za-zhangwenlei
- * @Date: Created in 9:51 2021/9/24
+ * @Date: Created in 9:44 2021/9/26
  */
-public class Test2 {
+public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("Test begin");
